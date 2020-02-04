1c86e10a7227a70d6ea2f2d32d2268848a6ef9ed
clw_work <clw_work@163.com>
2020-02-04 15:55:42 +0800

    完善了预览调试

--- js/render.js
+++ js/render.js
@@ -1,29 +1,26 @@
-function render(doc) {
+function render() {
   let html = editor.html.getValue()
-  let style = editor.css.getValue()
-  let script = editor.js.getValue()
-  if (doc.head) {
-    doc.head.innerHTML = ""
-  }
-  if (doc.body) {
-    doc.body.innerHTML = ""
-  }
-  doc.write(`
-  <!DOCTYPE html>
-  <html lang="zh">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>预览界面</title>
-    <style>
-    ${style}
-    </style>
-  </head>
-  <body>
-  ${html}
-  <script>${script}</script>
-  </body>
-  </html>
-  `)  
+  let css = editor.css.getValue()
+  let js = editor.js.getValue()
+  localStorage.setItem('code-html', html)
+  localStorage.setItem('code-css', css)
+  localStorage.setItem('code-js', js)
+  // doc.write(`
+  // <!DOCTYPE html>
+  // <html lang="zh">
+  // <head>
+  //   <meta charset="UTF-8">
+  //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  //   <title>预览界面</title>
+  //   <style>
+  //   ${style}
+  //   </style>
+  // </head>
+  // <body>
+  // ${html}
+  // <script>${script}</script>
+  // </body>
+  // </html>
+  // `)  
 }
